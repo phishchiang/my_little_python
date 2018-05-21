42ce06f3b4283d343cb4906ffc69bbb700159cd3
Phish Chiang <phishchiang@gmail.com>
2018-05-21 19:53:16 +0800

    Added some old C's string formatting comparison

--- f_strings.py
+++ f_strings.py
@@ -1,5 +1,6 @@
 my_name = 'Phish'
 my_age = '30'
+my_num = 50
 my_work = 'animator'
 
 # print (f'Hello, {my_name}. I\'m {my_age} years old. 5 years later.')
@@ -7,9 +8,13 @@ my_work = 'animator'
 # print (f'Hello, {my_name}.'+ '\n' + f'I\'m {my_age} years old. 5 years later.')
 
 # to show braces {}
+
 print(
   f'Hi {my_name}.'
   f'You are a {my_work}.' 
   f'You\'re {my_age} old.'
 )
 print(f'{my_name}')
+
+# Old C's String Formatting Operator
+print("This %d is a %s pen!" %(my_num, my_work))
